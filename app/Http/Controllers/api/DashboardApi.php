78ae860f6585e6918a7958c4f85aadafdbb6e94e
satiya3577@gmail.com <satiya3577@gmail.com>
2021-03-12 13:44:12 +0700

    Update API Top10_ICD10

--- app/Http/Controllers/api/DashboardApi.php
+++ app/Http/Controllers/api/DashboardApi.php
@@ -19,6 +19,8 @@ class DashboardApi extends Controller
                 left join f_diag_icd10_type on t_diag_icd10.f_diag_icd10_type_id = f_diag_icd10_type.f_diag_icd10_type_id
                 where
                 t_diag_icd10.f_diag_icd10_type_id = '1' 
+                and t_diag_icd10.diag_icd10_number not like '%O80%'
+                and t_diag_icd10.diag_icd10_number not like '%Z%'
                 and t_visit.f_visit_status_id <> '4'
                 and t_visit.f_visit_type_id = '1'
                 and (substring(visit_financial_discharge_time,1,10) between substring('2563-10-01',1,10) and substring('2564-09-30',1,10))
